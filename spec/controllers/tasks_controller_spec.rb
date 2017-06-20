7ff1ad2f7a8ea6da1933a65010e69a719e4934af
Dennis Ouyang <den2net@yahoo.com>
2017-06-20 19:29:04 +0000

    passed tests for task create

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -24,4 +24,14 @@ RSpec.describe TasksController, type: :controller do
       expect(task1.done).to eq true
     end
   end
+
+  describe "tasks#create" do
+    it "should allow new tasks to be created" do
+      post :create, params: { task: { title: "fix things" } }
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("fix things")
+      expect(Task.last.title).to eq("fix things")
+    end
+  end
 end
